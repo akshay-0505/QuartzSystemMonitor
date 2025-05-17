0371d59a238f6d42df39c716acd3f6d29881b333
akshay-0505 <akshaygadhave531@gmail.com>
2025-05-17 19:10:04 +0530

    commiting template

---
+++ src/test/java/com/practice/SystemMonitor/SystemMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.SystemMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
